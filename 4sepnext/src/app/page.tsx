ff3b5c7325ff8447c10acc0cc9f653a8408d8360
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-04 18:48:20 +0500

    Update page.tsx

--- 4sepnext/src/app/page.tsx
+++ 4sepnext/src/app/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+ import React from 'react'
 
 export default function Home() {
+  let baseURl = ""
+
+  fetch(`${baseUrl}/books`)
   return (
     <div className='justify-center  text-center flex'>
       <h1 className='p-4'>Home</h1></div>
