a1684752c74cee43bfe538c50de54adbd000b6ca
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-04 22:54:28 +0500

    updated -bg/ text

--- 4sepnext/src/app/page.tsx
+++ 4sepnext/src/app/page.tsx
@@ -1,40 +1,30 @@
 import React from "react";
 
 interface IBook {
-  id:number,
-  name:string,
-  type:string,
-  available:boolean,
-
-
+  id: number;
+  name: string;
+  type: string;
+  available: boolean;
 }
 
 async function Home() {
   let baseURL = "https://simple-books-api.glitch.me";
 
   let res = await fetch(`${baseURL}/books`);
-  let books:IBook = await res.json();
+  let books: IBook = await res.json();
   // console.log(books);
 
-
   return (
-    <div className="justify-center  text-center flex">
-      <h1 className="p-4">Home
-
-      </h1>
-      <ul>
-        <li>
-        books[0].name</li>
-        <li>
-        books[1].name</li>
-        <li>
-        books[2].name</li>
-        <li>
-        books[3].name</li>
-        <li>
-        books[4].name</li>
-        <li>
-        books[5].name</li>
+    <div className="justify-center text-center text-white ">
+      <h1 className=" text-4xl
+       justify-center p-5">API INVOKING</h1>
+      <ul className="text-2xl p-9 ">
+        <li>{books[1].name}</li>
+        <li>{books[2].name}</li>
+        <li>{books[3].name}</li>
+        <li>{books[4].name}</li>
+        <li>{books[0].name}</li>
+        <li>{books[5].name}</li>
       </ul>
     </div>
   );
