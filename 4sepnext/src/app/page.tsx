2c20b4a10c6ccb4e2b267f8329bebb585911448d
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-04 18:55:07 +0500

    Update page.tsx

--- 4sepnext/src/app/page.tsx
+++ 4sepnext/src/app/page.tsx
@@ -1,11 +1,24 @@
- import React from 'react'
+import React from "react";
 
-export default function Home() {
-  let baseURl = ""
-
-  fetch(`${baseUrl}/books`)
-  return (
-    <div className='justify-center  text-center flex'>
-      <h1 className='p-4'>Home</h1></div>
-  )
+interface IBook{
+  
 }
+
+ async function Home() {
+  let baseURl = "https://simple-books-api.glitch.me";
+
+  let res = await fetch(`${baseUrl}/books`);
+  let books = await res.json();
+  console.log(books);
+
+  books[0].name
+
+  return (
+    <div className="justify-center  text-center flex">
+      <h1 className="p-4">Home</h1>
+    </div>
+  );
+}
+
+
+export default Home
