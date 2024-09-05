b1c0dee8b4c16e618a0b2c32932ef1eee673353c
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-05 09:45:04 +0500

    Update orderbutton.tsx

--- 4sepnext/src/app/orderbutton.tsx
+++ 4sepnext/src/app/orderbutton.tsx
@@ -1,22 +1,21 @@
 "use client"
-import React from "react";
 
-function OrderButton () => {
+import { placeOrder } from "./utilis"
 
-let onOrderPlace = async () => {
+function OrderButton() {
 
-    await placeOrder()
-   
+    let onOrderPlace = async () => {
+        // Use server function
+        await placeOrder()
+    }
+
+    return (
+        <button onClick={onOrderPlace} className="mx-4 p-2 bg-blue-500">
+            Order
+        </button>
+    )
 }
- return (
-    <button>Order</button>
- )
 
-}
-
-export default OrderButton;
-
-
-
+export default OrderButton
 
 // A util file is where you keep reusable helper functions or tools that are used in different parts of your code, making your code cleaner and more organized.
