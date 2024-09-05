2b05811fab30581df2d123f1ce1c12895e71faf0
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-05 11:06:51 +0500

    Update utilis.ts

--- 4sepnext/src/app/utilis.ts
+++ 4sepnext/src/app/utilis.ts
@@ -6,17 +6,31 @@ export let placeOrder = async () => {
     customerName: "TanzeelAhmad",
   };
 
-//   OrderPlace API calling
+  //   OrderPlace API calling
 
-let res = await fetch ("https://simple-books-api.glitch.me/orders",{
-    method:"POST",
-    body:JSON.stringify(orderdata),
-    headers:{
-        Authorization:``
-    }
-})
- let data = await res.json()
- console.log(data)
-
-
+  let res = await fetch("https://simple-books-api.glitch.me/orders", {
+    method: "POST",
+    body: JSON.stringify(orderdata),
+    headers: {
+      Authorization: `c48784d4e79eb40582d60378a59a7c2cf1dc2d877f62a19d07c4d243f78ceaea`,
+    },
+  });
+  let data = await res.json();
+  console.log(data);
 };
+
+
+
+// {
+    
+//     "clientName":"RAZA",
+//     "clientEmail": "Quant@example.com"
+
+    
+// }
+
+
+// {
+//     "created": true,
+//     "orderId": "vVBpimDRA24N2FzvHKz-P"
+// }
